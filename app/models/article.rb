c4900b62a68df688a888230be633ef243fb63433
Development-Urquidi <pro.urquidi@gmail.com>
2016-12-19 16:05:06 -0600

    Fixed welcome page by defining index within the welcome controller; finalized post deletion functionality; began comment functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,3 +2,9 @@ class Article < ApplicationRecord
   validates :title, presence: true,
                     length: {minimum: 5}
 end
+
+class Comment < ApplicationRecord
+  has_many :comments
+  validates :title, presence: true,
+                    length: {minimum: 5 }
+end
